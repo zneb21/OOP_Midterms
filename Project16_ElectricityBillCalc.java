4d5dbb797145e21880ab023337134c8bd412b894
frenchmarballescas-wq <331283101+Frenchwhoopwhoop@users.noreply.github.com>
2026-09-21 00:20:23 +0800

    little bit of 16

--- Project16_ElectricityBillCalc.java
+++ Project16_ElectricityBillCalc.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Project16_ElectricityBillCalc {
     public static void main(String[] args) {
-        // Your code here
-    }
+       
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number of customers: ");
+        int customers = input.nextInt();
+
+        for (int customer = 1; customer <= customers; customer++) {
+
+            System.out.print("Enter consumption of electricity of a customer " + customer + ": ");
+            double consumption = input.nextDouble();
+
+
+
+            
+  }
+ }
 }
